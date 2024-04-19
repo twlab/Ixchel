3aac512fd51f87134a771313af77a26a23935bab
Juan Macias <44846802+juanfmacias@users.noreply.github.com>
2024-04-18 23:21:50 -0500

    Adding in annotation splitting

--- SourceCode/Ixchel.py
+++ SourceCode/Ixchel.py
@@ -69,7 +69,6 @@ def extract_cytosine_annotations(args):
     # use system commands to report some statistics
     ## How many segments are there?
     segments_count = count_lines(input_file)
-    print(f"\n")
     print(f"    Number of segments in {input_file}: {segments_count}")
     ## How many Cytosine annotations need to be converted?
     annotations_count = count_lines(output_file)
