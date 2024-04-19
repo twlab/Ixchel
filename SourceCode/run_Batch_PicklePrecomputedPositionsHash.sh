bc4c4bb1f55e55f92f025be54f21e48229486849
Juan Macias <44846802+juanfmacias@users.noreply.github.com>
2024-04-19 00:47:43 -0500

    Adding in annotation splitting

---
+++ SourceCode/run_Batch_PicklePrecomputedPositionsHash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --mem=8G
+#SBATCH -n 1
+#SBATCH -N 1
+
+read INPUTPRECOMPUTEDFILE < <( sed -n ${SLURM_ARRAY_TASK_ID}p $1 )
+
+echo "Loading software..."
+eval $( spack load --sh /pudl6n3 )
+eval $( spack load --sh py-jsonpickle@1.4.1 )
+eval $( spack load --sh py-dill@0.3.4 )
+
+#INPUTPRECOMPUTEDFILE=$1
+#OUTPUTPICKLEFILE=$2
+
+echo "Input precomputed file: $INPUTPRECOMPUTEDFILE"
+
+echo "Start python script..."
+python3 -u /scratch/hllab/Juan/Ixchel/SourceCode/Ixchel.py SerializePrecomputedPositionsHash $INPUTPRECOMPUTEDFILE
+echo "Python script complete!"
