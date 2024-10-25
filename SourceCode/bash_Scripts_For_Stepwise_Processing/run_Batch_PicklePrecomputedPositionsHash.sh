27e1ef4bfdf3fa2c4bd799fe91e12096991fe426
Juan Macias <44846802+juanfmacias@users.noreply.github.com>
2024-10-25 14:34:14 -0500

    Cleanup

--- SourceCode/bash_Scripts_For_Stepwise_Processing/run_Batch_PicklePrecomputedPositionsHash.sh
+++ SourceCode/bash_Scripts_For_Stepwise_Processing/run_Batch_PicklePrecomputedPositionsHash.sh
@@ -10,9 +10,6 @@ eval $( spack load --sh /pudl6n3 )
 eval $( spack load --sh py-jsonpickle@1.4.1 )
 eval $( spack load --sh py-dill@0.3.4 )
 
-#INPUTPRECOMPUTEDFILE=$1
-#OUTPUTPICKLEFILE=$2
-
 echo "... Input precomputed file: $INPUTPRECOMPUTEDFILE"
 
 echo "... Start script..."
